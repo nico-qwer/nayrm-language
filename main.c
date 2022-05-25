0e698cd828c0c0816e78c4be04b0d30e091ece62
nico_qwer <nicolas.hebert@outlook.com>
2022-05-24 22:18:09 -0400

    Opening file done

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Counts the lines in a file
+int countLines(char fileName[]) {
+    FILE* sourceCodeStream = fopen(fileName, "r"); //Open file
+
+    int lines = 0;
+    int ch = 0;
+
+    //Code snippet from https://stackoverflow.com/questions/12733105/c-function-that-counts-lines-in-file
+    while (!feof(sourceCodeStream)) {
+        ch = fgetc(sourceCodeStream);
+        if (ch == '\n') {
+            lines++;
+        }
+    }
+    //End of code snippet
+
+    fclose(sourceCodeStream); //Close file
+    return lines;
+}
+
+int main(int argc, char* argv[]) {
+    FILE* sourceCodeStream = fopen(argv[1], "r"); //Open file
+
+    if (sourceCodeStream == NULL) { perror("Unable to open file."); return 1; } //Make sure file is valid
+
+    char sourceCode[countLines(argv[1])][255]; //Array of the lines of the file
+
+    //Stores file contents in source code variable
+    int i = 0;
+    while (fgets(sourceCode[i], sizeof(sourceCode[0]), sourceCodeStream)) { i++; }
+
+    fclose(sourceCodeStream); //Close file
+
+    return 0;
+}
